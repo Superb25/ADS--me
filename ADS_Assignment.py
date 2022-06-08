9f241cf1e7acd7648993698160277cfe267426d8
Superb25 <103606544+Superb25@users.noreply.github.com>
2022-06-09 00:15:42 +0100

    Add files via upload

---
+++ ADS_Assignment.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title='Superb Dash', page_icon='random', layout='centered', initial_sidebar_state='auto')
+
+path = "./Assignment/dataonline.csv"
+st.sidebar.title('Superb Dash')
+Assignment = st.sidebar.file_uploader('Upload Dataset', type=['csv'])
+
+
+
